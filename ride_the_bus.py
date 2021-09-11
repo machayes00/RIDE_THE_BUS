e8e6a1cddb02ff1ba8f50f6c0d9876d370e38c2e
Mac Hayes <machayes00@gmail.com>
2021-09-11 13:47:37 -0500

    started 'ride the bus' file

---
+++ ride_the_bus.py
@@ -0,0 +1,46 @@
+import random
+
+
+guess = " "
+points = 0
+wins = 0
+losses = 0
+gamesPlayed = 0
+winPercentage = 0
+for j in range(0,10):
+    while(gamesPlayed != 10000):
+        arr = []
+        for x in range(1,53):
+            arr.append(x)
+
+        random.shuffle(arr)
+        deck = arr[:42]
+        pyramid = arr[42:]
+        points = 0
+        gamesPlayed +=1
+
+        for x in range(0, 42):
+            if(pyramid[points] < 7):
+                guess = "HIGHER"
+            elif(pyramid[points] > 7):
+                guess = "LOWER"
+            else:
+                choice = random.randrange(0,1)
+                if(choice == 1):
+                    guess = 'HIGHER'
+                else:
+                    guess = 'LOWER'
+            if((guess == "HIGHER" and deck[0] > pyramid[0]) or (guess =="LOWER" and deck[0] < pyramid[0])):
+                pyramid[points] = deck[0]
+                points+=1
+            else:
+                pyramid[points] = deck[0]
+                points = 0 
+            if(points == 10):
+                wins+=1
+                break
+            deck.pop(0)
+    winPercentage+=((wins/gamesPlayed)*100)
+    print((wins/gamesPlayed)*100)
+
+print("Games Played:", 100000, "Win Percentage:", winPercentage/10, "%")
