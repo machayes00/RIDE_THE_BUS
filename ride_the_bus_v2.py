57c821774b5c1d36e308575ce2d3e2386c1afb9f
Mac Hayes <machayes00@gmail.com>
2021-09-17 22:51:56 -0500

    Implemented a 'score' variable. Uncertain if it actually works correctly.

--- ride_the_bus_v2.py
+++ ride_the_bus_v2.py
@@ -5,9 +5,9 @@ def ride_the_bus(gamesToPlay, numberOfCardsInPyramid):
     wins = 0
     gamesPlayed = 0
     winPercentage = 0
-
+    avgScore = 0
+    totalScore = 0
     while(gamesPlayed != gamesToPlay):
-        
         pyramidCards = numberOfCardsInPyramid
         deckCards = 52 - pyramidCards
         guess = " "
@@ -21,14 +21,13 @@ def ride_the_bus(gamesToPlay, numberOfCardsInPyramid):
         pyramid = arr[deckCards:]
         gamesPlayed +=1
 
-        for x in range(deckCards):
+        for x in range(deckCards): #plays the pyramid
             if(pyramid[points] < 7):
                 guess = "HIGHER"
             elif(pyramid[points] > 7):
                 guess = "LOWER"
             else:
-                choice = random.randrange(0,1)
-                if(choice == 1):
+                if(random.randrange(0,1)): #guesses a number between 0 and 1
                     guess = 'HIGHER'
                 else:
                     guess = 'LOWER'
@@ -37,33 +36,35 @@ def ride_the_bus(gamesToPlay, numberOfCardsInPyramid):
                 points+=1
             else:
                 pyramid[points] = deck[0]
-                points = 0 
+                points = 0
+                totalScore+=1
             if(points == numberOfCardsInPyramid):
                 wins+=1
                 break
             deck.pop(0)
     winPercentage=(wins/gamesPlayed)*100
+    avgScore = int(totalScore/gamesPlayed)
 
-    print("Games Played:", gamesPlayed, "Win Percentage:", winPercentage, "%")
+    print("Games Played: ", gamesPlayed, "Win Percentage: ", winPercentage, "%", "Average Score: ", avgScore)
 
 def playGame():
     try:
         games = int(input("Enter how many games the computer should play: "))
         assert(games>0 and games<=1000000), "Invalid number of games"
+    except TypeError:
+        print("Invalid type")
+        exit()    
     except Exception as e:
         print(e)
         exit()   
-    except TypeError:
-        print("Invalid type")
-        exit()
     try:
         pyramid = int(input("Enter how many cards to be in the pyramid: "))
         assert(pyramid > 0 and pyramid <= 26), "Invalid number of cards in Pyramid, must be at size(deck)/2"
-    except Exception as e:
-        print(e)
-        exit()
     except TypeError:
         print("Invalid Type")
+        exit()
+    except Exception as e:
+        print(e)
         exit()
     
     ride_the_bus(games, pyramid)
